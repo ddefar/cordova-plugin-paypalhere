0f4339865d6298b179cef14ddc0af29fd7c14ae9
doris defar <ddefar@doriss-MacBook-Pro.local>
2015-09-17 15:32:14 +0200

    wrapped invoice with invoiceData

--- www/PayPalHere.js
+++ www/PayPalHere.js
@@ -18,17 +18,18 @@ var payPalHere = {
         );
     },
 
-    makePayment: function(invoice, successCallback, errorCallback) {
+    makePayment: function(invoiceData, successCallback, errorCallback) {
     	
-        var payer = encodeURIComponent("some@email.dom");
-        var invoiceId = encodeURIComponent("123456");
-        var number = encodeURIComponent("0987");
-        var uri = "butterapp//";
+        var payer = encodeURIComponent(invoiceData.payer);
+        var invoiceId = encodeURIComponent(invoiceData.invoiceId);
+        var number = encodeURIComponent(invoiceData.number);
+        var uri = invoiceData.uri;
+        var invoice = invoiceData.invoice;
 
         var retUrl = encodeURIComponent(uri + "?{result}?Type={Type}&InvoiceId={InvoiceId}&Tip={Tip}&Email={Email}&TxId={TxId}");
         var pphereUrl = "paypalhere://takePayment?accepted=" + encodeURIComponent("cash,card,paypal") + "&returnUrl=" + retUrl;
         pphereUrl += "&InvoiceId=" + invoiceId + "&as=b64&payerPhone=5551234567&step=choosePayment" + "&invoice=" + btoa(JSON.stringify(invoice));
-
+console.log('*****', pphereUrl);
 		cordova.exec(
             successCallback,
             errorCallback,
